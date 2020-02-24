d91fae4586fafbe2a26e961477bd8dd5c8d673d8
Paul Campbell <animalcarpet@gmail.com>
2020-02-24 22:18:37 +0000

    fix path

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,11 +3,11 @@
 mysql < ../schema/mydlm.sql
 mysql < ../data/standard_data.sql
 mysql < ../routines/mydlm_routines.sql
-mysql < ../routines/mydlm_events.sql
+mysql < ../events/mydlm_events.sql
 
 mysql -e 'SELECT @@GLOBAL.event_scheduler';
 mysql -e 'SET GLOBAL event_scheduler = ON;';
 
-echo "Run CALL `mydlm`.`import_schemata` to auto-populate the `schemata` table"
-echo "Run CALL `mydlm`.`import_tables('schema_name')` to auto-populate tables"
+echo "Run CALL mydlm.import_schemata to auto-populate the schemata table"
+echo "Run CALL mydlm.import_tables('schema_name') to auto-populate tables"
 
